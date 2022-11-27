650985ed149c67ac93fc262820ff7b49143f6a2b
Neeraj <neginee01raj@gmil.com>
2022-11-27 23:04:37 +0530

    Added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,18 +2,20 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price =0;
     private Boolean isVeg;
     private String bill;
+    boolean extracheese = false;
+    boolean extratopping = false;
+    boolean takeaway = false;
 
     public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
         if (isVeg) {
-            System.out.println("Base Price Of The Pizza: 300");
+           // System.out.println("Base Price Of The Pizza: 300");
             price+= 300;
         } else {
-            System.out.println("Base Price Of The Pizza: 400");
-            price+= 400;
+                 price+= 400;
         }
     }
 
@@ -22,28 +24,46 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        System.out.println("Extra Cheese Added: 80");
-          price+= 80;
+        //System.out.println("Extra Cheese Added: 80");
+         if(!extracheese) {
+             price+= 80;
+             extracheese = true;
+         }
     }
 
-    public void addExtraToppings(){
-       if(isVeg){
-           System.out.println("Extra Toppings Added: 70");
-           price+= 70;
-       }
-       else {
-           System.out.println("Extra Toppings Added: 120");
-           price+= 120;
-       }
+    public void addExtraToppings() {
+        if (!extratopping) {
+            if (isVeg) {
+               // System.out.println("Extra Toppings Added: 70");
+                price+= 70;
+            } else {
+                //System.out.println("Extra Toppings Added: 120");
+                price+= 120;
+            }
+        }
+        extratopping = true;
     }
 
-    public void addTakeaway(){
-        System.out.println("Paperbag Added: 20");
-        price+=20;
+    public void addTakeaway() {
+      //  System.out.println("Paperbag Added: 20");
+        if (!takeaway) {
+            price += 20;
+            takeaway = true;
+        }
     }
 
     public String getBill(){
-        bill = "Total Price: "+this.getPrice();
-          return this.bill;
+        if(isVeg)    System.out.println("Base Price Of The Pizza: 300");
+         else        System.out.println("Base Price Of The Pizza: 400");
+if(extracheese)  System.out.println("Extra Cheese Added: 80");
+if(extratopping) {
+    if(isVeg)  System.out.println("Extra Toppings Added: 70");
+
+    else  System.out.println("Extra Toppings Added: 120");
+}
+if(takeaway)         System.out.println("Paperbag Added: 20");
+
+        bill = "Total Price: "+this.getPrice()+"\n";
+          return bill;
     }
 }
